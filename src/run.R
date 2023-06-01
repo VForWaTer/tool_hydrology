5a075ed38f3e5d2528f78d9f9d12a618d7a84101
Mirko Mälicke <mirko@maelicke-online.de>
2023-06-01 12:18:43 +0200

    implement baseflow_seperation

--- src/run.R
+++ src/run.R
@@ -1,6 +1,12 @@
 # load json2aRgs for parameter parsing
 library(json2aRgs)
 
+# load all needed third party libraries
+library(grwat)
+
+# make the tool functions available
+source('functions.R')
+
 # get the call parameters for the tool
 params <- get_parameters()
 
@@ -10,29 +16,15 @@ toolname <- tolower(Sys.getenv("TOOL_RUN"))
 # extract the discharge timeseries
 df = params$discharge 
 
-# Install the third party R packages
-install.packages('grwat')
-
-# make functions from main file available
-source('functions.R')
-
-# make entire workflows from  file available
-source('workflows.R')
 
 if (toolname == "") {
-    toolname <- "foobar"
+    toolname <- "baseflow_seperation"
 }
 
 # Switch for the different tools available in this package
-if (toolname == "foobar") {
-    # RUN the tool here and create the output in /out
+if (toolname == "baseflow_seperation") {
+    baseflow <- baseflow_separation(df)
 
-    # prints to STDOUT.log
-    print("You have tried to run the tool 'foobar'.
-This tool is the template tool without any functionality.
-Please implement another tool or select the tool you 
-have already implemented.")
-    print(params)
 } else {
     # in any other case, the tool was invalid or not configured
     print(paste("[", Sys.time(), "] Either no TOOL_RUN environment variable available, or '", toolname, "' is not valid.\n", sep = ""))
